a5fbcdc6b9f548ef9b7d33ac39586f57cb5c91bf
David Mendoza <davidmendozaofficial@gmail.com>
2024-08-13 14:10:47 +0800

    fix
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -11,14 +11,14 @@ class PatientController extends Controller
     function webhook(Request $req){
         $patient = new Patient();
 
-        $patient->idNumber = $req->idNumber;
-        $patient->dateOfExpiry = $req->dateOfExpiry;
-        $patient->fullName = $req->fullName;
-        $patient->dateOfBirth = $req->dateOfBirth;
-        $patient->address = $req->address;
-        $patient->placeOfIssue = $req->placeOfIssue;
-        $patient->yearOfBirth = $req->yearOfBirth;
-        $patient->nationality = $req->nationality;
+        $patient->idNumber = $req['idNumber'];
+        $patient->dateOfExpiry = $req['dateOfExpiry'];
+        $patient->fullName = $req['fullName'];
+        $patient->dateOfBirth = $req['dateOfBirth'];
+        $patient->address = $req['address'];
+        $patient->placeOfIssue = $req['placeOfIssue'];
+        $patient->yearOfBirth = $req['yearOfBirth'];
+        $patient->nationality = $req['nationality'];
         $patient->data = $req->all();
 
         if($patient->save()){

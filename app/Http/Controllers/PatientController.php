a615172e9c76d1cc803d80a044dbbbf2efc1a5b8
David Mendoza <davidmendozaofficial@gmail.com>
2024-08-15 10:37:31 +0800

    fix
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -54,10 +54,10 @@ class PatientController extends Controller
             $temp = [];
             $temp2 = json_decode($patient->data);
 
-            $temp->code = $temp2->code;
-            $temp->name = $temp2->name;
-            $temp->gender = $temp2->gender;
-            $temp->birthday = $temp2->birthday;
+            $temp->code = $temp2['code'];
+            $temp->name = $temp2['name'];
+            $temp->gender = $temp2['gender'];
+            $temp->birthday = $temp2['birthday'];
             $temp->actions = "test";
 
             array_push($array, $temp);
